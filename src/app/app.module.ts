ad7d932fee2cbd0e985c8e462bd87bffa82845f8
dazyang <dazyang@protonmail.com>
2020-07-17 16:13:14 +0800

    added media item components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
+import { MediaItemComponent } from "./media-item.component";
 
 
 @NgModule({
   // bring in other Angular module that the module will need
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule],
   // makes components, directive and pipes available to your module that don't come from another module
   declarations: [
-    AppComponent
-  ],
+    AppComponent,
+    MediaItemComponent],
   // use for the root module and will let angular know which component or components for the bootstrap process
   //(entry point for the app code)
-  bootstrap: [
-    AppComponent
-  ],
+  bootstrap: [AppComponent],
 }) // the decorator NgModule will apply to the class, field or methods that comes before
 export class AppModule {}
