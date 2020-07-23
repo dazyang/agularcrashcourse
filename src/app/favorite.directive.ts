ded5561acd5ef1207088183acec56a4046088e79
dazyang <dazyang@protonmail.com>
2020-07-23 13:16:00 +0800

    added comments

--- src/app/favorite.directive.ts
+++ src/app/favorite.directive.ts
@@ -1,9 +1,14 @@
-import { Directive, HostBinding } from '@angular/core'
+import { Directive, HostBinding, Input } from '@angular/core'
 
 @Directive({
  selector: '[mwFavorite]'
 })
 export class FavoriteDirective {
   // class is referring to a native DOM property available on elements
-  @HostBinding('class.is-foavorite') isFavorite = true
+  @HostBinding('class.is-favorite') isFavorite = true
+  // Setter method will be called when a property with the same name is set to a value from an instense of a class
+  // a setter method will get pass a value
+  @Input() set mwFavorite(value) {
+    this.isFavorite = value
+  }
 }
