de15cdcfbe5a2ac3d252ab7b41899559537c5de2
dazyang <dazyang@protonmail.com>
2020-07-27 16:22:01 +0800

    event bidning disabled

--- src/app/media-item-form.component.ts
+++ src/app/media-item-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'mw-media-item-form',
@@ -11,10 +11,16 @@ export class MediaItemFormComponent implements OnInit {
 
   ngOnInit() {
     this.form = new FormGroup({
-      medium: new FormControl('Movies'),
-      name: new FormControl(''),
-      category: new FormControl(''),
-      year: new FormControl(''),
+      medium: new FormControl("Movies"),
+      name: new FormControl(
+        "",
+        Validators.compose([
+          Validators.required,
+          Validators.pattern("[\\w\\-\\s\\/]+"),
+        ])
+      ),
+      category: new FormControl(""),
+      year: new FormControl(""),
     });
   }
 
