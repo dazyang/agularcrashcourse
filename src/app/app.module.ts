5dd57de19b5e9305b5eef2fa90cb37cfe2594a0d
dazyang <dazyang@protonmail.com>
2020-07-30 17:30:57 +0800

    created, imported, commented a custom service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { MediaItemListComponent } from './media-item-list.component';
 import { FavoriteDirective } from './favorite.directive';
 import { CategoryListPipe } from './category-list.pipe';
 import { MediaItemFormComponent } from './media-item-form.component';
+import { MediaItemService } from './media-item.service'
 
 @NgModule({
   imports: [
@@ -23,6 +24,10 @@ import { MediaItemFormComponent } from './media-item-form.component';
   ],
   bootstrap: [
     AppComponent
+  ],
+  // this will tell angular to instantiate an instance of the service for use by things in this ngModule and any ngModules down the tree
+  providers: [
+    MediaItemService
   ]
 })
 export class AppModule {}
